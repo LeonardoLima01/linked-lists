9d3ab6f74cb34bf3e946da0dbb048f7b161d5d4e
socry <leonardosbo02@gmail.com>
2023-03-17 22:12:59 -0300

    Add linked list class

--- solutions.js
+++ solutions.js
@@ -4,3 +4,23 @@ class Node {
     this.next = next;
   }
 }
+
+class LinkedList {
+  constructor() {
+    this.listHead = null;
+    this.listSize = 0;
+  }
+  append(value) {
+    if (this.listHead == null) {
+      this.listHead = new Node(value);
+    } else {
+      let current = this.listHead;
+
+      while (current.next != null) {
+        current = current.next;
+      }
+      current.next = new Node(value);
+    }
+    this.listSize++;
+  }
+}
